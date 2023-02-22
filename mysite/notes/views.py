0abfe3b85d09680e57695270e7aa22d4c3d543c1
Alex McDonald <alexmcdonald712@gmail.com>
2023-02-22 15:53:12 -0700

    corrected auth requirement

--- mysite/notes/views.py
+++ mysite/notes/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
 from .forms import NoteEditor
 
 # Create your views here.
 def notes(response):
-    id = response.user.id
-    note_db = Note.objects.all()
-    notes = {}
-    for note in note_db:
-        if note.user_id == id:
-            notes[note.title] = note.content
-    form = NoteEditor()
-    return render(response, "notes/notes.html", {"form":form, "notes":notes})
+    if response.user.is_authenticated:
+        id = response.user.id
+        note_db = Note.objects.all()
+        notes = {}
+        for note in note_db:
+            if note.user_id == id:
+                notes[note.title] = note.content
+        form = NoteEditor()
+        return render(response, "notes/notes.html", {"form":form, "notes":notes})
+    return redirect('/login')
