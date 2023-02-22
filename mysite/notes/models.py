44b1d10f7a3726131520f23d1d3be93fb85c0037
Alex McDonald <alexmcdonald712@gmail.com>
2023-02-22 09:24:09 -0700

    integrated text editor to db

--- mysite/notes/models.py
+++ mysite/notes/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="note", null=True)
     title = models.CharField(max_length=200)
-    notes = models.CharField(max_length=10000)
+    notes = HTMLField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
