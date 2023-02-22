179c5f29838508aa53c010fa419ca2d769e89003
Alex McDonald <alexmcdonald712@gmail.com>
2023-02-22 11:02:55 -0700

    added home page

--- mysite/notes/views.py
+++ mysite/notes/views.py
@@ -4,6 +4,10 @@ from .models import Note
 from .forms import NoteEditor
 
 # Create your views here.
-def index(response):
+def home(response):
+    return render(response, "notes/home.html", {})
+
+def notes(response, id):
+    notes = Note.objects.all()
     form = NoteEditor()
-    return render(response, "notes/notes.html", {"form":form})
+    return render(response, "notes/notes.html", {"form":form, "notes":notes})
